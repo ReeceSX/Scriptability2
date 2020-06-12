a20552642a81112bd1d6faba66db352d7c653397
Reece W <me@reece.sx>
2020-06-12 17:33:32 +0100

    Update common headers

---
+++ Include/ScriptabilityCommon.hpp
@@ -0,0 +1,15 @@
+/***
+    Copyright (©) 2020 Reece Wilson (a/k/a "Reece"). All rights reserved.
+    Do not use, copy, distribute, publish, disseminate, modify, or sublicense without express permission from the rights holder[s].
+    Please do no evil.
+
+    File: ScriptabilityCommon.hpp
+    Date: 2020-6-10
+    Originator: Reece
+    Purpose:
+***/
+#pragma once
+
+#include "ScriptabilityConfiguration.hpp"
+#include "ScriptabilityEnvrionment.hpp"
+#include "ScriptabilityTypes.hpp"
